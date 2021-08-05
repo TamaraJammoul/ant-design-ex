c1f0b71153dd9bce5114f0cb213149c72a2b2975
TamaraJamoul <tamara.jamoul@wizme.com>
2021-08-05 21:52:49 +0300

    ant design example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { DetailsComponent } from './details/details.component';
+import { TableComponent } from './table/table.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { RouterModule, Routes } from '@angular/router';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzImageModule } from 'ng-zorro-antd/image';
+
+
+registerLocaleData(en);
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TableComponent,
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    TableComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzTableModule,
+    NzDividerModule,
+    NzModalModule,
+    NzDescriptionsModule,
+    NgbModule,
+    NzTypographyModule,
+    NzProgressModule,
+    NzCarouselModule,
+    NzBreadCrumbModule,
+    NzListModule,
+    NzImageModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  exports: [RouterModule],
 })
 export class AppModule { }
---
+++ src/app/details/details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+  array = [1, 2, 3, 4];
+  effect = 'scrollx';
+
+  data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.'
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
